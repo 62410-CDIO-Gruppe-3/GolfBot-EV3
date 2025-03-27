f06a961e55128a4d5d52a9665b712a0f248e4313
Hatal24 <s224523@dtu.dk>
2025-03-27 14:15:06 +0100

    added VIPBall class file

---
+++ src/PathFinding/VIPBall.py
@@ -0,0 +1,18 @@
+from src.PathFinding.CartesianCoordinateSystem import CartesianCoordinateSystem
+from src.PathFinding.Point import Point
+
+
+class VIPBall:
+    def __init__(self, point):
+        self.point = point
+
+    def place_in_system(self, coordinate_system):
+        self.point.place_in_system(coordinate_system)
+        print(f"Placed ball at: {coordinate_system.get_coordinate()}")
+
+
+# Example usage
+coordinate_system = CartesianCoordinateSystem(1800, 1200)
+point = Point(100, 200)
+ball = VIPBall(point)
+ball.place_in_system(coordinate_system)
