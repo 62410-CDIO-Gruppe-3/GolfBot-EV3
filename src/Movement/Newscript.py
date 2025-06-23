41943bcb6ab16d2d94f474c3c797b8e424928996
root3008 <bleeding.30.8@gmail.com>
2025-06-23 14:03:16 +0200

    moves test work

--- src/Movement/Newscript.py
+++ src/Movement/Newscript.py
@@ -18,7 +18,7 @@ MAX_RETRIES = 3             # Maximum number of connection retries
 
 # Constants for robot movement
 
-ANGLE = 360
+ANGLE = 45
 DISTANCE = 100
 
 # ----------------------------------------------------------------------
@@ -101,12 +101,26 @@ def main() -> None:
     response = send_and_receive("print('Hello from EV3')\n")
     print("Response from EV3:", response)
     time.sleep(1)
+    
     response = send_and_receive(CLOCKWISE_REVOLUTION)
     print("Response from EV3:", response)
     time.sleep(4)
+    
+    response = send_and_receive(MOVE_FORWARD)
+    print("Response from EV3:", response)
+    time.sleep(1)
+    
     response = send_and_receive(COUNTERCLOCKWISE_REVOLUTION)
     print("Response from EV3:", response)
     time.sleep(1)
+    
+    response = send_and_receive(MOVE_BACKWARD)
+    print("Response from EV3:", response)
+    time.sleep(1)
+    
+    """
+
+
     response = send_and_receive(OPEN_GATE)
     print("Response from EV3:", response)
     #time.sleep(1)
@@ -119,7 +133,21 @@ def main() -> None:
     response = send_and_receive(CLOSE_GATE)
     print("Response from EV3:", response)
     
-"""
+    response = send_and_receive(OPEN_GATE)
+    print("Response from EV3:", response)
+    #time.sleep(1)
+    
+    
+    
+    response = send_and_receive(MOVE_FORWARD)
+    print("Response from EV3:", response)
+    time.sleep(1)
+    
+
+    
+response = send_and_receive(CLOSE_GATE)
+print("Response from EV3:", response)
+    
 response = send_and_receive(CLOCKWISE_REVOLUTION)
 print("Response from EV3:", response)
 time.sleep(1)
@@ -128,5 +156,6 @@ response = send_and_receive(COUNTERCLOCKWISE_REVOLUTION)
 print("Response from EV3:", response)
 time.sleep(1)
 """
+
 if __name__ == "__main__":
     main()
